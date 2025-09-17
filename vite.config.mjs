0ee1813c66272b661701256a64901ecc41430c0f
willdunklin <will.dunklin@kitware.com>
2025-09-17 13:56:12 -0400

    Update vite base

--- vite.config.mjs
+++ vite.config.mjs
@@ -11,7 +11,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/beccah_resume/',
+  base: '/resume/',
   plugins: [
     VueRouter(),
     Vue({
